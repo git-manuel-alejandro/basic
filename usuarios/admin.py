1e024331b8bc852d232c831eee7fbd6459c8f2d6
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-01-20 14:02:41 -0300

    settings static media files

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Ciudad , Usuario
 # Register your models here.
+
+admin.site.register(Ciudad)
+admin.site.register(Usuario)
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,33 @@
+from distutils.command import upload
+from email import charset
+from pyexpat import model
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+
+class Ciudad(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'ciudades'
+        verbose_name_plural = 'ciudades'
+
+class Usuario(models.Model):
+
+    genero_eleccion = (
+        ('M' , 'Masculino'),
+        ('F' , 'Femenino')
+    )
+    codigo = models.IntegerField()
+    photo = models.ImageField(upload_to = 'photos/%Y/%m/%d/')
+    nombre = models.CharField(max_length=100)
+    profesion = models.CharField(max_length=100)
+    genero = models.CharField(choices=genero_eleccion , max_length=100)
+    ciudad = models.ForeignKey(Ciudad , on_delete=models.PROTECT)
+
+    def __str__(self):
+        return (self.nombre)
